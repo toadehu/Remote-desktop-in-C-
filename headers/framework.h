4ebe3da4049fa1da53dd02568e3b65d16f07e89a
Razvan Puscasu <65716317+toadehu@users.noreply.github.com>
2023-04-24 18:27:39 +0200

    Create framework.h

---
+++ headers/framework.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdbool.h>
+#include <time.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_timer.h>
+#include <SDL_ttf.h>
+#include "graphics_objects.h"
+#include "server.h"
+#include "graphics_renderer.h"
+#include "screen_capture.h"
+#include "video_encoder.h"
+#include "video_decoder.h"
+#include "basic_video.h"
