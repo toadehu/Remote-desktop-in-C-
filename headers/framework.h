b06ac0faae58128b40700c86f2bd887a9e9e8361
Razvan Puscasu <65716317+toadehu@users.noreply.github.com>
2023-07-16 19:51:35 +0300

    Update framework.h

--- headers/framework.h
+++ headers/framework.h
@@ -6,6 +6,7 @@
 #include <SDL_image.h>
 #include <SDL_timer.h>
 #include <SDL_ttf.h>
+#include <SDL_syswm.h>
 #include "graphics_objects.h"
 #include "server.h"
 #include "graphics_renderer.h"
@@ -15,6 +16,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+
+#define CLICK_LEFT 1
+#define CLICK_RIGHT 2
+#define CLICK_MIDDLE 3
+#define CLICK_OEM1 5
+#define CLICK_OEM2 6
 
 #ifdef _WIN32
 #pragma comment(lib, "msvcrt.lib")
